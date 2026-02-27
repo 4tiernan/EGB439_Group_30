34e4af84ade14f03c9c49d07705f975edcf206b8
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-02-27 10:44:52 +1000

    Implemented colour printing and improved wifi manager

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import wifi_manager  
 from pibot import pibot_client
 import time
+from colour_printing import print_coloured, bcolors
+
 
 wifi_manager.assert_connection_to_network("EGB439") # Ensure you are connected to the EGB439 network.
 
@@ -8,10 +10,14 @@ bot = pibot_client.PiBot(ip="172.19.232.120")
 
 
 def main_loop():
-    time.sleep(1)
+    bot.setVelocity(20,20,1)
+    print(bot.getEncoders())
 
 try:
     while True:
         main_loop()
+        bot.setVelocity(0,0)
+        time.sleep(1)
+
 except KeyboardInterrupt:
     wifi_manager.assert_connection_to_network("QUT")
--- wifi_manager.py
+++ wifi_manager.py
@@ -2,6 +2,7 @@ import subprocess
 import re
 import os
 import time
+from colour_printing import print_coloured, bcolors
 
 def get_windows_ssid():
     try:
@@ -14,6 +15,7 @@ def get_windows_ssid():
     
 def connect_to_wifi(ssid_name):
     # Command to connect to a known network profile by name
+    print_coloured(f"You are not connected to the {ssid_name} network. Trying to connect automatically...", bcolors.OKCYAN)
     command = f'cmd /c "netsh wlan connect name={ssid_name}"'
     os.system(command)
     
@@ -21,14 +23,16 @@ def connect_to_wifi(ssid_name):
     timeout = 10 # Seconds
     while(time.time()-start < timeout):
         if(get_windows_ssid() == ssid_name):
-                return f"Successfully connected to {ssid_name} network."
+                print_coloured(f"Successfully connected to {ssid_name} network.", bcolors.OKBLUE)
+                return
     
-    return f"Failed to connect to the {ssid_name} network after {timeout} seconds."
+    print_coloured(f"Failed to connect to the {ssid_name} network after {timeout} seconds.", bcolors.FAIL)
+    return
 
 def assert_connection_to_network(ssid):
     if(get_windows_ssid() != ssid):
-        print(f"You are not connected to the {ssid} network. Trying to connect automatically...")
-        print(connect_to_wifi(ssid))
+        
+        connect_to_wifi(ssid)
     
 
 
