b0e0c855a227297fd3d4bd45ae763b7042f05fda
Brad E <Bradroy830@gmail.com>
2026-02-27 10:51:12 +1000

    just some more playing around

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from pibot_client import PiBot
+from pibot_client import *
+
 
 bot = PiBot(ip="172.19.232.120")
 
-# testing new branch features
 
-# good function to merge
+
+# testing wheels for 3 seconds
+# bot.setVelocity(right_motor, left_motor, duration=if needed)
+# anticlockwise is negative, clockwise is position, currently using PWM signals.
+# bot.setVelocity(-50, -50, duration=3)
+print("Wheels set to 50 for 3 seconds")
+bot.setVelocity(100, 100, 7, 3)
+bot.stop()
+
