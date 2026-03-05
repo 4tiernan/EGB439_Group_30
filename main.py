150d150136eb7b1a05a4e1b85600ae1dacbe2bb5
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-03-05 16:41:37 +1000

    Semi implemented navigator and added code for localiser and modified sim for real time sim using main.py

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import wifi_manager  
 from pibot.pibot_client import PiBot
+from pibot.pibot_sim import PiBotSim
 import time
+import numpy as np
 from colour_printing import print_coloured, bcolors
 from navigation import navigate
 
+use_simulation = True
 on_campus = True
-if(wifi_manager.get_windows_ssid() != "QUT" and wifi_manager.get_windows_ssid() != "EGB439"): # If not connected to QUT or EGB439, try to connect to the penquin pi network.
-    on_campus = False
+if(not use_simulation):
+    if(wifi_manager.get_windows_ssid() != "QUT" and wifi_manager.get_windows_ssid() != "EGB439"): # If not connected to QUT or EGB439, try to connect to the penquin pi network.
+        on_campus = False
 
-if(on_campus):
-    wifi_manager.assert_connection_to_network("EGB439") # Ensure you are connected to the EGB439 network.
-    bot = PiBot(ip="172.19.232.120")
-else:
-    wifi_manager.assert_connection_to_network("penguinpi:07:c5:ca")
-    bot = PiBot(ip="10.42.0.1")
+    if(on_campus):
+        wifi_manager.assert_connection_to_network("EGB439") # Ensure you are connected to the EGB439 network.
+        bot_ip = "172.19.232.120"
+        localiser_ip = "172.19.232.104"
+        
+    else:
+        wifi_manager.assert_connection_to_network("penguinpi:07:c5:ca")
+        bot_ip = "10.42.0.1"
+        localiser_ip = None
+
+    bot = PiBot(ip=bot_ip, localiser_ip=localiser_ip)
+
+else: # Using Simulator
+    print_coloured("Running in SIMULATION mode. No real robot will be controlled.", bcolors.WARNING)
+    bot = PiBotSim(
+        pose=np.array([0.5, 0.5, 0.0]),
+        dt=0.05,
+        realtime=False,  # True means run as fast as possible for testing)
+    )
 
 bot.resetEncoder()
 #time.sleep(1)
@@ -21,19 +38,34 @@ bot.resetEncoder()
 #print(f"Voltage: {bot.getVoltage()}")
 
 
-def main_loop():
-    bot.move(0.1, duration=1)
+def update_control():
     current_pose = bot.getLocalizerPose(group_number=30)
+    current_pose = (0,0,0) if current_pose is None else current_pose # If localizer fails, assume we are at the origin facing right (0 radians).
+
     target_pose = (1, 1, 0) # Example target pose (x, y, theta)
     velocity_commands = navigate(current_pose, target_pose)
+    print(f"Current Pose: {current_pose}, Target Pose: {target_pose}, Velocity Commands: {velocity_commands}")
+    bot.move(*velocity_commands)
 
+def main_loop():
+    last_loop_time = time.time()
+    last_sim_time = time.time()
+    while True:
+        if(time.time() - last_loop_time >= 0.5): # Run the main loop at 2 Hz
+            update_control()
+            last_loop_time = time.time()
+        
+        if(use_simulation and time.time() - last_sim_time >= 0.05): # Update the simulation at 20 Hz for smooth animation
+            bot.update_simulation()
+            last_sim_time = time.time()
 
 try:
-    while True:
-        main_loop()
-        time.sleep(0.1)
+    main_loop()
 
 except KeyboardInterrupt:
-    bot.stop()
-    if(on_campus):
+    if(use_simulation):
+        bot.stop_simulation()
+    else: 
+        bot.stop()
+    if(on_campus and not use_simulation):
         wifi_manager.assert_connection_to_network("QUT")
--- navigation.py
+++ navigation.py
@@ -1,5 +1,18 @@
+import numpy as np
 def navigate(from_pose, to_pose):
-    # Placeholder implementation - replace with actual navigation logic
-    forward_vel = 0.1
-    angular_vel = 0.05
+    # Simple heading controller
+    dist_to_target = np.sqrt((to_pose[0] - from_pose[0])**2 + (to_pose[1] - from_pose[1])**2)
+    desired_heading = np.arctan2(to_pose[1] - from_pose[1], to_pose[0] - from_pose[0])
+    heading_error = desired_heading - from_pose[2]
+
+    forward_vel_gain = 0.5
+    angular_vel_gain = 1
+
+    heading_correction = 0
+    if(dist_to_target < 0.1): # If we are close to the target, focus on correcting heading rather than moving forward.
+        forward_vel_gain = 0
+        heading_correction = 3 * (to_pose[2] - from_pose[2]) # Add a correction term to help turn in place when close to the target.
+
+    forward_vel = forward_vel_gain * dist_to_target    
+    angular_vel = angular_vel_gain * heading_error + heading_correction
     return (forward_vel, angular_vel)
